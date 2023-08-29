11e0d832af58c90142f7bde7d343b256245909f6
Shatodru <shatodru2003@gmail.com>
2023-08-29 09:33:37 +0530

    E1

---
+++ script.js
@@ -0,0 +1,54 @@
+var timer = 60
+var score = 0
+var hitrn = 0
+
+
+
+function increaseScore() {
+    score += 10
+    document.querySelector("#scoreValue").textContent = score
+}
+
+
+function getNewHit() {
+    hitrn = Math.floor(Math.random() * 10)
+    document.querySelector("#hitValue").textContent = hitrn
+}
+
+
+function makeBubble() {
+    var clutter = ""
+    for (let i = 1; i <= 160; i++) {
+        var rn = Math.floor(Math.random() * 10)
+        clutter += `<div class="bubble">${rn}</div>`
+    }
+    document.querySelector("#pbtm").innerHTML = clutter
+}
+
+
+function runTimer() {
+    var timerint = setInterval(function () {
+        if (timer > 0) {
+            timer--;
+            document.querySelector("#TimerValue").textContent = timer;
+        } else {
+            clearInterval(timerint)
+            document.querySelector("#pbtm").innerHTML = `<h1>Game has ended</h1>`
+        }
+    }, 1000)
+}
+
+
+document.querySelector("#pbtm").addEventListener("click", function (dets) {
+    var clickedNum = Number(dets.target.textContent)
+    if (clickedNum === hitrn) {
+        increaseScore()
+        makeBubble()
+        getNewHit()
+    }
+})
+
+runTimer()
+makeBubble()
+getNewHit()
+increaseScore()
